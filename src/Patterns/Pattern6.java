d135dbfb82d90d86ff8d080a2ddd7e6d4d044c00
SonamAhirwar01 <ahirwarsonam01@gmail.com>
2026-07-04 23:36:24 +0530

    Added  more pattern problems

---
+++ src/Patterns/Pattern6.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= 5; i++) {
+            //for space
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            //for Star
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            //for space
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
